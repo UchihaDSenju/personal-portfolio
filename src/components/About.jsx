8f0850a89316261a43df0aab08a8ec493e7ed8bf
Tariq <rafiqtariq19@gmail.com>
2024-03-11 15:08:17 +0530

    responsiveness added to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,17 @@ import { data } from '../constants'
 
 function About() {
   return (
-    <div className='flex w-full justify-evenly my-5'>
-      <div className='w-[50%] flex font-caveat justify-center items-center flex-col text-4xl'>
-        <div className='leading-[4rem] flex flex-col'>
-          <div className='text-center'>Hi I am Mohamed Tariq</div>
-          <div className='font-spaceGrotesk text-center'>A Full Stack Web Developer</div>
-          <div className='text-center w-[70%] mx-auto'>{data.aboutDesc.desc}</div>
+    <div className='flex flex-col-reverse w-full justify-evenly my-5 sm:flex-row'>
+      <div className='sm:w-[50%] flex font-caveat justify-center items-center flex-col text-4xl'>
+        <div className='leading-[4rem] flex flex-col mt-7 sm:mt-0'>
+          <div className='text-center text-5xl'>Hi I am Mohamed Tariq</div>
+          <div className='font-spaceGrotesk text-center mt-5'>A Full Stack Web Developer</div>
+          <div className='text-center sm:w-[70%] mx-auto'>{data.aboutDesc.desc}</div>
           <a href={data.aboutDesc.resumeLink} target='_blank' className='mx-auto'><button className='red-btn'>View My Resume</button></a>
         </div>
       </div>
-      <div className='w-[50%] flex flex-col justify-center items-center'>
-        <div className='bg-white w-[500px] h-[500px] rounded-full'><img src={data.aboutDesc.profile} className='object-cover w-[100%] h-[100%] rounded-full'></img></div>
+      <div className='sm:w-[50%] flex flex-col justify-center items-center'>
+        <div className='bg-white w-[450px] h-[450px] rounded-full sm:w-[500px] sm:h-[500px]'><img src={data.aboutDesc.profile} className='object-cover w-[100%] h-[100%] rounded-full'></img></div>
       </div>
     </div>
   )
